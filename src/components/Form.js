8fea467e8195bd844a78d3dcb5f74a7073646b4f
charles-okunzo <okunzopeter01@gmail.com>
2023-07-07 11:53:53 +0300

    Form submission for todo item

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,27 @@
 import styles from "../style.module.css"
 
 
-const Form = ()=>{
+const Form = ({todo, setTodo, todoList, setTodoList})=>{
+    const handleChange = (event)=>{
+        setTodo(event.target.value)
+    }
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        // const tempList = todoList
+        // tempList.push(todo)
+        // setTodoList(tempList)
+        setTodoList([...todoList, todo])
+        setTodo("")
+        console.log(todoList)
+    }
+
     return(
         <div className={styles.todoform}>
-            <form>
-                <input className={styles.todoinput} placeholder="Add Todo Item"></input>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={todo} onChange={handleChange} className={styles.todoinput} placeholder="Add Todo Item"></input>
                 <br />
-                <button className={styles.todobutton}>ADD</button>
+                <button type="submit" className={styles.todobutton}>ADD</button>
             </form>
         </div>
     )
