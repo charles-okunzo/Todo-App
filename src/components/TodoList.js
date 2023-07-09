e7e1c26f6160df455f568582beedb187a8c2cbcc
charles-okunzo <okunzopeter01@gmail.com>
2023-07-09 18:36:44 +0300

    Add Todo Items header

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,9 +3,12 @@ import styles from "../style.module.css"
 
 const TodoList = ({todoList, setTodoList})=>{
     return (
+        <div>
+            <h2>Todo Items</h2>
         <div className={styles.todolist}>{todoList.map((todoItem, index)=>(
             <Todo key={todoItem.id} todoItem={todoItem} todoList={todoList} setTodoList={setTodoList} index={index}/>
         ))}</div>
+        </div>
     )
 }
 
